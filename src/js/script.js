ad8637eb5b243eefd1a80a0910be411c40815602
kozim-rasulov <kozim_7484443@mail.ru>
2021-07-01 22:30:21 +0500

    v1.4.5 Catalog Complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+
+                         /* Menu */
+
+    $('ul.menu').on('click', 'li:not(.menu__page_active)', function() {
+      $(this)
+        .addClass('menu__page_active').siblings().removeClass('menu__page_active')
+    });
+
                         /* Табы */
 
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
@@ -7,30 +15,23 @@ $(document).ready(function(){
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
         .closest('section.catalog').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
+    
+});
 
-                        /* Menu */
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu'),
+  menuItem = document.querySelectorAll('.menu_page'),
+  hamburger = document.querySelector('.hamburger');
 
-    $('ul.menu').on('click', 'li:not(.menu__page_active)', function() {
-      $(this)
-        .addClass('menu__page_active').siblings().removeClass('menu__page_active')
-    });
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
+  });
 
-                /* Якорем */
-
-    /* $('ul.menu').on('click', 'li:not(.menu__link_active)', function() {
-      $(this)
-      .addClass('menu__link_active').siblings().removeClass('menu__link_active')
-      
-    });
- 
-    var tabIndex = window.location.hash.replace('#tab','')-1;
-    if (tabIndex != -1) $('ul.menu li').eq(tabIndex).click();
- 
-    $('a[href*=#tab]').click(function() {
-      var tabIndex = $(this).attr('href').replace(/(.*)#tab/, '')-1;
-      $('ul.menu li').eq(tabIndex).click();
-    }); */
-
-
-
-}); 
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('menu_active');
+      })
+  })
+})
